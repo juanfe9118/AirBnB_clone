04fd0908aaeef3e53c12348e61c93c1b8b89e799
juanfe9118 <1168@holbertonschool.com>
2020-02-18 18:25:11 -0500

    Remove module and fix the implementation

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ BaseModel """
 from datetime import datetime
-from dateutil import parser
 import models
 import uuid
 
@@ -13,7 +12,8 @@ class BaseModel():
         if kwargs:
             for k, v in kwargs.items():
                 if k == 'created_at' or k == 'updated_at':
-                    setattr(self, k, parser.parse(v))
+                    setattr(self,
+                            k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, k, v)
         else:
