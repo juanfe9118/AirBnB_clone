c90561a80b951af77145fbb1210e72906399d7ed
juanfe9118 <1168@holbertonschool.com>
2020-02-18 18:14:36 -0500

    Remove a method

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,6 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.__class__ = __class__.__name__
             models.storage.new(self)
 
     def __str__(self):
@@ -29,7 +28,7 @@ class BaseModel():
         for k, v, in self.__dict__.items():
             if k != "__class__":
                 dic[k] = v
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, dic)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, dic)
 
     def save(self):
         """ Update and save object """
@@ -45,7 +44,3 @@ class BaseModel():
             else:
                 dic[k] = v
         return dic
-
-    def __class__(self):
-        """ Format name to string """
-        return str(__class__.__name__)
