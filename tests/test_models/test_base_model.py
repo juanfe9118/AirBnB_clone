4ad7d5e98de4683c9093c0f2dd573eaf8c2194ee
juanfe9118 <1168@holbertonschool.com>
2020-02-18 17:50:06 -0500

    Add more test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 import unittest
 import datetime
+import os
 
 
 class Test_Base(unittest.TestCase):
@@ -14,14 +15,28 @@ class Test_Base(unittest.TestCase):
         self.assertIsInstance(a, BaseModel)
 
     def test_obj_to_dict(self):
-        """ Test if is an instance """
+        """ Test if is an instance and how it print """
         a = BaseModel()
         self.assertIsInstance(a.to_dict(), dict)
+        for k, v in a.to_dict().items():
+            f = 0
+            if k == '__class__':
+                f = 1
+            if k == 'created_at':
+                self.assertIsInstance(v, str)
+            if k == 'updated_at':
+                self.assertIsInstance(v, str)
+        self.assertEqual(f, 1)
 
     def test_str_rep(self):
         """ Test string representation """
         a = BaseModel()
         self.assertIsInstance(a.__str__(), str)
+        dic = a.__dict__
+        del dic['__class__']
+        s1 = a.__str__()
+        s2 = '[BaseModel] ({}) {}'.format(a.id, dic)
+        self.assertEqual(s1, s2)
 
     def test_id(self):
         """ Test if each ID is different """
@@ -41,3 +56,28 @@ class Test_Base(unittest.TestCase):
         first = a.updated_at
         a.save()
         self.assertNotEqual(first, a.updated_at)
+
+    def test_permissions(self):
+        """ Test Permissions """
+        # Read permissions
+        r = os.access('models/base_model.py', os.R_OK)
+        self.assertTrue(r)
+        # Write permissions
+        w = os.access('models/base_model.py', os.W_OK)
+        self.assertTrue(r)
+        # Execute permissions
+        e = os.access('models/base_model.py', os.X_OK)
+        self.assertTrue(r)
+
+    def test_create_kw(self):
+        dic = {"id": "cec8988f-182e-45ce-934b-1e85aedf55c3"}
+        dic["created_at"] = "2020-02-18T13:34:09.711961"
+        dic["updated_at"] = "2020-02-18T13:34:09.711965"
+        dic["__class__"] = "BaseModel"
+        a = BaseModel(**dic)
+        cr = datetime.datetime(2020, 2, 18, 13, 34, 9, 711961)
+        up = datetime.datetime(2020, 2, 18, 13, 34, 9, 711965)
+        self.assertEqual(a.id, "cec8988f-182e-45ce-934b-1e85aedf55c3")
+        self.assertEqual(cr, a.created_at)
+        self.assertEqual(up, a.updated_at)
+        self.assertEqual(a.__class__, "BaseModel")
