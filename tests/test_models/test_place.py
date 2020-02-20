0e6464587b2ea983b2c06369655de8d229e2d032
juanfe9118 <1168@holbertonschool.com>
2020-02-19 19:28:14 -0500

    Modified tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,90 +1,50 @@
 #!/usr/bin/python3
-""" Test of Place """
+
+""" Module to test Place """
+
+
 from models.place import Place
 import unittest
-import datetime
+import models
 import os
 
 
-class Test_Place(unittest.TestCase):
-    """ Test of Place """
+class TestPlace(unittest.TestCase):
+    """ Test of File Storage """
 
-    def test_creation(self):
-        """ Test creation of an object """
-        a = Place()
-        self.assertIsInstance(a, Place)
+    def setUp(self):
+        """SetUp method"""
 
-    def test_obj_to_dict(self):
-        """ Test if is an instance and how it print """
-        a = Place()
-        self.assertIsInstance(a.to_dict(), dict)
-        for k, v in a.to_dict().items():
-            f = 0
-            if k == '__class__':
-                f = 1
-            if k == 'created_at':
-                self.assertIsInstance(v, str)
-            if k == 'updated_at':
-                self.assertIsInstance(v, str)
-        self.assertEqual(f, 1)
+        self.file_storage = Place()
 
-    def test_str_rep(self):
-        """ Test string representation """
-        a = Place()
-        self.assertIsInstance(a.__str__(), str)
-        dic = a.__dict__
-        s1 = a.__str__()
-        s2 = '[Place] ({}) {}'.format(a.id, dic)
-        self.assertEqual(s1, s2)
+    def TearDown(self):
+        """TearDown method."""
 
-    def test_id(self):
-        """ Test if each ID is different """
-        a = Place()
-        b = Place()
-        self.assertNotEqual(a.id, b.id)
-
-    def test_date_time(self):
-        """ Test if update and create are datetime """
-        a = Place()
-        self.assertIsInstance(type(a.created_at), type(datetime.datetime))
-        self.assertIsInstance(type(a.updated_at), type(datetime.datetime))
-
-    def test_save(self):
-        """ Test if updated_at is working correctly"""
-        a = Place()
-        first = a.updated_at
-        a.save()
-        self.assertNotEqual(first, a.updated_at)
-
-    def test_permissions(self):
-        """ Test Permissions """
-        # Read permissions
-        r = os.access('models/place.py', os.R_OK)
-        self.assertTrue(r)
-        # Write permissions
-        w = os.access('models/place.py', os.W_OK)
-        self.assertTrue(w)
-        # Execute permissions
-        e = os.access('models/place.py', os.X_OK)
-        self.assertTrue(e)
-
-    def test_create_kw(self):
-        """Test kwargs"""
-        dic = {"id": "cec8988f-182e-45ce-934b-1e85aedf55c3",
-               "created_at": "2020-02-18T13:34:09.711961",
-               "updated_at": "2020-02-18T13:34:09.711965"}
-        a = Place(**dic)
-        cr = datetime.datetime(2020, 2, 18, 13, 34, 9, 711961)
-        up = datetime.datetime(2020, 2, 18, 13, 34, 9, 711965)
-        self.assertEqual(a.id, "cec8988f-182e-45ce-934b-1e85aedf55c3")
-        self.assertEqual(cr, a.created_at)
-        self.assertEqual(up, a.updated_at)
-        self.assertEqual(a.__class__.__name__, "Place")
+        del self.file_storage
 
     def test_doc(self):
         """Test docs for class"""
-        self.assertIsNotNone(Place.__doc__)
-        a = Place()
-        self.assertIsNotNone(a.__str__.__doc__)
-        self.assertIsNotNone(a.save.__doc__)
-        self.assertIsNotNone(a.to_dict.__doc__)
+
+        self.assertIsNotNone(
+            models.place.__doc__,
+            "No docstring in the module"
+        )
+        self.assertIsNotNone(Place.__doc__, "No docstring in the class")
+
+    def test_permissions(self):
+        """Test Permissions of file"""
+
+        r = os.access('models/place.py', os.R_OK)
+        self.assertTrue(r, "Read permissions")
+        w = os.access('models/place.py', os.W_OK)
+        self.assertTrue(w, "Write permissions")
+        e = os.access('models/place.py', os.X_OK)
+        self.assertTrue(e, "Execute permissions")
+
+    def test_type(self):
+        """Test type of class"""
+
+        self.assertEqual(
+            str(type(self.file_storage)),
+            "<class 'models.place.Place'>")
+        self.assertIsInstance(self.file_storage, Place)
--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,90 +1,50 @@
 #!/usr/bin/python3
-"""Test module of State"""
+
+""" Module to test State """
+
+
 from models.state import State
 import unittest
-import datetime
+import models
 import os
 
 
-class Test_State(unittest.TestCase):
-    """Test class of State"""
+class TestState(unittest.TestCase):
+    """ Test of File Storage """
 
-    def test_creation(self):
-        """ Test creation of an object """
-        a = State()
-        self.assertIsInstance(a, State)
+    def setUp(self):
+        """SetUp method"""
 
-    def test_obj_to_dict(self):
-        """ Test if is an instance and how it print """
-        a = State()
-        self.assertIsInstance(a.to_dict(), dict)
-        for k, v in a.to_dict().items():
-            f = 0
-            if k == '__class__':
-                f = 1
-            if k == 'created_at':
-                self.assertIsInstance(v, str)
-            if k == 'updated_at':
-                self.assertIsInstance(v, str)
-        self.assertEqual(f, 1)
+        self.file_storage = State()
 
-    def test_str_rep(self):
-        """ Test string representation """
-        a = State()
-        self.assertIsInstance(a.__str__(), str)
-        dic = a.__dict__
-        s1 = a.__str__()
-        s2 = '[State] ({}) {}'.format(a.id, dic)
-        self.assertEqual(s1, s2)
+    def TearDown(self):
+        """TearDown method."""
 
-    def test_id(self):
-        """ Test if each ID is different """
-        a = State()
-        b = State()
-        self.assertNotEqual(a.id, b.id)
-
-    def test_date_time(self):
-        """ Test if update and create are datetime """
-        a = State()
-        self.assertIsInstance(type(a.created_at), type(datetime.datetime))
-        self.assertIsInstance(type(a.updated_at), type(datetime.datetime))
-
-    def test_save(self):
-        """ Test if updated_at is working correctly"""
-        a = State()
-        first = a.updated_at
-        a.save()
-        self.assertNotEqual(first, a.updated_at)
-
-    def test_permissions(self):
-        """ Test Permissions """
-        # Read permissions
-        r = os.access('models/state.py', os.R_OK)
-        self.assertTrue(r)
-        # Write permissions
-        w = os.access('models/state.py', os.W_OK)
-        self.assertTrue(w)
-        # Execute permissions
-        e = os.access('models/state.py', os.X_OK)
-        self.assertTrue(e)
-
-    def test_create_kw(self):
-        """Test kwargs"""
-        dic = {"id": "cec8988f-182e-45ce-934b-1e85aedf55c3",
-               "created_at": "2020-02-18T13:34:09.711961",
-               "updated_at": "2020-02-18T13:34:09.711965"}
-        a = State(**dic)
-        cr = datetime.datetime(2020, 2, 18, 13, 34, 9, 711961)
-        up = datetime.datetime(2020, 2, 18, 13, 34, 9, 711965)
-        self.assertEqual(a.id, "cec8988f-182e-45ce-934b-1e85aedf55c3")
-        self.assertEqual(cr, a.created_at)
-        self.assertEqual(up, a.updated_at)
-        self.assertEqual(a.__class__.__name__, "State")
+        del self.file_storage
 
     def test_doc(self):
         """Test docs for class"""
-        self.assertIsNotNone(State.__doc__)
-        a = State()
-        self.assertIsNotNone(a.__str__.__doc__)
-        self.assertIsNotNone(a.save.__doc__)
-        self.assertIsNotNone(a.to_dict.__doc__)
+
+        self.assertIsNotNone(
+            models.state.__doc__,
+            "No docstring in the module"
+        )
+        self.assertIsNotNone(State.__doc__, "No docstring in the class")
+
+    def test_permissions(self):
+        """Test Permissions of file"""
+
+        r = os.access('models/state.py', os.R_OK)
+        self.assertTrue(r, "Read permissions")
+        w = os.access('models/state.py', os.W_OK)
+        self.assertTrue(w, "Write permissions")
+        e = os.access('models/state.py', os.X_OK)
+        self.assertTrue(e, "Execute permissions")
+
+    def test_type(self):
+        """Test type of class"""
+
+        self.assertEqual(
+            str(type(self.file_storage)),
+            "<class 'models.state.State'>")
+        self.assertIsInstance(self.file_storage, State)
